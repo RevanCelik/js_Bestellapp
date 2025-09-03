b06f618044186fd91ee5f176a15078c699baba24
Revan Celik <revancelik444@gmail.com>
2025-09-03 07:53:25 +0200

    js ausgelagert

--- script.js
+++ script.js
@@ -31,26 +31,6 @@ function basketDishes() {
     basketFullPrice();
 }
 
-function getBasketTemplate(i) {
-    let fullPrice = basket[i].price * basket[i].count;
-
-    return `
-    <div>
-            <strong>${basket[i].name}</strong>
-        <div class="basket_style">
-            <div class="basket_center">
-                <button onclick="decreaseDish(${i})" class="basket_button_style">-</button>${basket[i].count}x <button onclick="increaseDish(${i})" class="basket_button_style">+</button>
-            </div>
-            <div class="basket_center">
-                ${fullPrice.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })}
-                <button class="material-icons" onclick="deleteDish(${i})">&#xe872;</button>
-            </div>
-        </div>
-    
-    </div>
-    `;
-}
-
 function basketFullPrice() {
     let basketFullPriceContentRef = document.getElementById('basket_total_price');
     basketFullPriceContentRef.innerHTML = "";
---
+++ script/template.js
@@ -0,0 +1,19 @@
+function getBasketTemplate(i) {
+    let fullPrice = basket[i].price * basket[i].count;
+
+    return `
+    <div>
+            <strong>${basket[i].name}</strong>
+        <div class="basket_style">
+            <div class="basket_center">
+                <button onclick="decreaseDish(${i})" class="basket_button_style">-</button>${basket[i].count}x <button onclick="increaseDish(${i})" class="basket_button_style">+</button>
+            </div>
+            <div class="basket_center">
+                ${fullPrice.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })}
+                <button class="material-icons" onclick="deleteDish(${i})">&#xe872;</button>
+            </div>
+        </div>
+    
+    </div>
+    `;
+}
